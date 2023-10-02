3f7e6906b272d23447d1cc4bc5918ff8f71dcb73
Mrs-Brightside <taniascmarques.4@gmail.com>
2023-10-02 22:12:24 +0100

    creaction of playRound function

--- odin-rock-paper-scissores.js
+++ odin-rock-paper-scissores.js
@@ -13,4 +13,41 @@ function getComputerChoice () {
     return computerMove;
 }
 
-console.log(getComputerChoice());
+function playRound (playerSelection) {
+    
+    const computerMove = getComputerChoice();
+    
+    let result = '';
+    
+    if (playerSelection === 'rock' ) {
+        if (computerMove === 'rock') {
+            result = 'You tied.';
+        } else if (computerMove === 'paper') {
+            result = 'You Lose! Paper beats rock.'; 
+        } else if (computerMove === 'scissors') {
+            result = "You Win! Rock beats scissors.";
+        }
+
+    } else if  (playerSelection === 'paper' ) {
+        if (computerMove === 'rock') {
+            result = 'You Win! Paper beats rock.';
+        } else if (computerMove === 'paper') {
+            result = 'You Tied.'; 
+        } else if (computerMove === 'scissors') {
+            result = "You Lose! Scissors beats paper.";
+        }
+
+    } else if (playerSelection === 'scissors' ) {
+        if (computerMove === 'rock') {
+            result = 'You Lose! Rock beats scissors.';
+        } else if (computerMove === 'paper') {
+            result = 'You win! Scissors beats paper.'; 
+        } else if (computerMove === 'scissors') {
+            result = "You Tied.";
+        }
+    };
+
+    return result;
+};
+
+console.log(playRound('scissors'));
